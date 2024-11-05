f9084999d10e641bb3a94634340edebf0f7b5aad
andrea-altamirano <159966662+andrea-altamirano@users.noreply.github.com>
2024-11-05 00:29:32 +0000

    examen

--- src/prueba.cpp
+++ src/prueba.cpp
@@ -12,25 +12,33 @@ int main(int argc, char const *argv[])
 {
     std::string reset_position;
     int frame = 0;
-    auto can = Canvas(200, 200);
-    //can.DrawPointLine(0, 0, 10, 10);
-    ////can.DrawPointCircle(10, 10, 10);
-    can.DrawBlockCircle(100, 100, 50);
-    can.DrawBlockEllipse(80, 90, 5, 10);
-    can.DrawBlockEllipse(110, 90, 5, 10);
-    can.DrawPointLine(200, 100, 200, 200);
+    int frame_direction = 1;
+    const int frame_max = 5;
+    const int frame_min = 0;
+
     auto ColorSupport = vbox({});
+    
     while (true)
     {
+        auto can = Canvas(200, 200);
+        can.DrawBlockCircle(100, 100, 50);
+        can.DrawBlockEllipse(80, 90, 5, 10);
+        can.DrawBlockEllipse(110, 90, 5, 10);
+        can.DrawBlockCircle(100, 125, frame);
+
         Screen pantalla = Screen::Create(Dimension::Full(), Dimension::Full());
-        // Element personaje = spinner(20, frame);
         Element lienzo = bgcolor(Color::DarkRed, border(vbox(ColorSupport, canvas(&can))));
         Render(pantalla, lienzo);
         std::cout << reset_position;
         pantalla.Print();
         reset_position = pantalla.ResetPosition(true);
         this_thread::sleep_for(0.1s);
-        frame++;
+        if (frame >= frame_max)
+            frame_direction = -1;
+        if (frame <= frame_min)
+            frame_direction = 1;
+        frame += frame_direction;
+        
     }
 
     return 0;
